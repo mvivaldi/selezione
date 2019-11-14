97fafa47b9b6536b879e8275b07166c1e5d84755
matteopastorelli <57702585+matteopastorelli@users.noreply.github.com>
2019-11-14 23:39:07 +0100

    Create monitorfile.sh
    Script per monitoraggio file log access.log del servizio apache

---
+++ esercizio_1/monitorfile.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Nome:         monitorfile.sh
+# Autore:       Matteo Pastorelli
+# Data:         13/11/2019
+# Versione:     1.0
+# Utilizzo:     Per lanciare script al boot il file init di start-stop monitoraggio deve essere inserito nella dir /etc/init.d e va aggiornato rc.d con update-rc.d
+#               Per utilizzo singolo : nohup monitorfile.sh > /var/log/nohupmonitorfile.log &
+#
+# Note:
+# Modifiche:
+
+## -o-o-o-o- Init script da inserire in init.d
+##! /bin/sh
+## /etc/init.d/monitorfile
+##
+#
+#case "$1" in
+#  start)
+#    nohup monitorfile.sh > /var/log/nohupmonitorfile.log &
+#    ;;
+#  stop)
+#     pkill monitorfile.sh
+#    ;;
+#  *)
+#    echo "Usage: /etc/init.d/monitorfile {start|stop}"
+#    exit 1
+#    ;;
+#esac
+#
+#exit 0
+## -o-o-o-o- Fine init script
+
+
+# Inizializzazione Variabili
+FILE_LOG1=/var/log/apache/access_site_a.log
+FILE_LOG2=/var/log/apache/access_site_b.log
+FILE_LOG3=/var/log/apache/access_site_c.log
+
+webhooklog=/tmp/webhook.log
+
+#Verifica della presenza dei file di log
+if [ -e $FILE_LOG1 ]; then
+        echo $FILE_LOG1 ":file presente"
+fi
+if [ -e $FILE_LOG2 ]; then
+        echo $FILE_LOG2 ":file presente"
+fi
+if [ -e $FILE_LOG3 ]; then
+        echo $FILE_LOG3 ":file presente"
+fi
+
+tail -f $FILE_LOG1 -f $FILE_LOG2 -f $FILE_LOG3 | while read LINE; do
+
+        if echo $LINE | grep -q '" 404 ' ; then
+                echo "Errore 404 -- $LINE" >> $webhooklog
+                curl -i http://webhook.local/error_404
+        elif echo $LINE | grep -q '/404.html' ; then
+                echo "Pagina 404 -- $LINE" >> $webhooklog
+                curl -i http://webhook.local/pagina_404
+        fi
+
+done
